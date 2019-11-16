27815fc717c3762324f48e099696b443e5d94413
Miles <milesasp@gmail.com>
2019-11-16 12:24:06 +0800

    add async await

--- src/user/login.e2e-spec.ts
+++ src/user/login.e2e-spec.ts
@@ -1,16 +1,16 @@
 import { browser, by, element, $, $$, ExpectedConditions as EC } from 'protractor';
 
 fdescribe('the user try to login', () => {
-  beforeEach(() => {
-    browser.get('/');
+  beforeEach( async () => {
+    await browser.get('/');
   });
   
-  it('should login to event page', () => {
-    element(by.linkText('登入')).click();
-    element(by.id('userName')).sendKeys('John');
-    element(by.id('password')).sendKeys('123456');
-    element(by.buttonText('登入')).click();
-    expect(browser.getCurrentUrl()).toBe(browser.baseUrl + 'events');
+  it('should login to event page', async () => {
+    await element(by.linkText('登入')).click();
+    await element(by.id('userName')).sendKeys('John');
+    await element(by.id('password')).sendKeys('123456');
+    await element(by.buttonText('登入')).click();
+    expect(await browser.getCurrentUrl()).toBe(browser.baseUrl + 'events');
   });
 
 });
