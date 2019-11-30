0e9767f66b4f310e9dfae74a38bbb89d508962b7
Miles <milesasp@gmail.com>
2019-11-30 14:06:17 +0800

    實戰演練：練習遮罩動態圖的呈現測試

--- src/labs/golden-gif.e2e-spec.ts
+++ src/labs/golden-gif.e2e-spec.ts
@@ -1,4 +1,17 @@
+import { browser, element, by } from "protractor";
+import { addMask, compareScreenshot } from "blue-harvest";
 
-describe('gif page present', () => {
-
+fdescribe('gif page present', () => {
+    it('should compare gif lab page', async () => {
+        await browser.get('/labs/gif');
+        await browser.manage().window().setSize(1024, 768);
+        const golden = `goldens/giflab.png`;
+        const diffDir = 'goldens/';
+        const gif_img = element(by.id('gif-img'));
+        await addMask(gif_img, 'gray');
+        await browser.waitForAngular();
+        const actual = await browser.takeScreenshot();
+        const result = await compareScreenshot(actual, golden, diffDir);
+        expect(result).toBeTruthy();
+    });
 });
